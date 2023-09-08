a666680e873ab4de8a2acb5e9573738609192523
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-09-08 19:55:40 +0100

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(void) {
+
+int	main(void) {
   /* This function prints the string Programming is like building a multilingual puzzle  to the standard output stream. */
   puts("\"Programming is like building a multilingual puzzle");
   return 0;
