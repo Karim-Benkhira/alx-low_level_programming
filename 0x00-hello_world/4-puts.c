3f7743d2e680b8a03e7fdaeb88228c8b76ca6dac
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-09-08 20:37:16 +0100

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * This is a simple C program that demonstrates the use of the puts function.
+ * It prints a message to the console.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
