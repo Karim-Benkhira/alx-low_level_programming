ac44750c528c1daa23520f486a536941af6888ff
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-09-23 20:20:00 +0100

    print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints all
+ *lowercase and uppercase
+ * letters of the English alphabet followed by a newline character.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    char alpha_upper = 'A';
-    char alpha_lower = 'a';
-    while (alpha_lower <= 'z')
-    {
-        putchar(alpha_lower);
-        alpha_lower++;
-    }
-    while (alpha_upper <= 'Z')
-    {
-        putchar(alpha_upper);
-        alpha_upper++;
-    }
-    
-    putchar('\n');
-    return (0);
-    
+char alpha_upper = 'A';
+char alpha_lower = 'a';
+while (alpha_lower <= 'z')
+{
+putchar(alpha_lower);
+alpha_lower++;
 }
+while (alpha_upper <= 'Z')
+{
+putchar(alpha_upper);
+alpha_upper++;
+}
+putchar('\n');
+return (0);
+}
+
