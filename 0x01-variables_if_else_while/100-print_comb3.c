4d2d658ee5b52813c6aa7e4cf496dfd02092d5c4
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-09-30 13:23:44 +0100

    push

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,21 @@ int main(void)
 {
 int number_1 = 0;
 int number_2;
-while (number_1 < 10)
+while (number_1 < 9)
 {
-number_2 = 1;
+number_2 = number_1 + 1;
 while (number_2 < 10)
+{
+if (number_1 != number_2)
 {
 putchar('0' + number_1);
 putchar('0' + number_2);
+if (number_1 != 8 || number_2 != 9)
+{
 putchar(',');
 putchar(' ');
+}
+}
 number_2++;
 }
 number_1++;
