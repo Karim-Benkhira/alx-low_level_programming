72d0400e40991655fd768bdf1968958232e98ab8
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-11-18 02:48:28 +0100

    push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * ft_putchar - writes a character to stdout
@@ -12,47 +13,6 @@
 void ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-/**
- * ft_atoi - Converts a string to an integer
- * @str: The input string to be converted
- *
- * Return: The converted integer
- */
-int ft_atoi(char *str)
-{
-	int	i;
-	int	a;
-	int	s;
-
-	i = 0;
-	s = 1;
-	a = 0;
-	while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\r'))
-		str++;
-	while (str[i] == '+' || str[i] == '-')
-	{
-	if (str[i] == '-')
-		s = s * -1;
-	if (str[i] == '+')
-		s = s * 1;
-	else if (str[i] == '-' && str[i] == '-')
-		return (0);
-	else if (str[i] == '-' && str[i] == '+')
-		return (0);
-	else if (str[i] == '+' && str[i] == '+')
-		return (0);
-	else if (str[i] == '+' && str[i] == '-')
-		return (0);
-	str++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		a = (a * 10) + str[i] - '0';
-		str++;
-	}
-	return (a * s);
 }
 
 /**
@@ -72,8 +32,8 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	num1 = ft_atoi(argv[1]);
-	num2 = ft_atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
 	result = num1 * num2;
 	printf("%d\n", result);
